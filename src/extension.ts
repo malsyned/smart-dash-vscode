f3741d13ea43b2cde7f5456b45882113e7a45915
Dennis Lambe Jr <malsyned@malsyned.net>
2022-07-04 11:47:48 -0400

    extension.ts: use spaces for indentation

--- src/extension.ts
+++ src/extension.ts
@@ -1,137 +1,137 @@
 import {
-	commands,
-	extensions,
-	window,
-	ExtensionContext,
-	Position,
-	Range,
-	TextDocument,
-	workspace,
-	TextEditor,
+    commands,
+    extensions,
+    window,
+    ExtensionContext,
+    Position,
+    Range,
+    TextDocument,
+    workspace,
+    TextEditor,
 } from 'vscode';
 
 export function activate(context: ExtensionContext) {
-	registerCommand(context, 'smart-dash.insert', insert);
-	registerCommand(context, 'smart-dash.insertGreaterThan', insertGt);
+    registerCommand(context, 'smart-dash.insert', insert);
+    registerCommand(context, 'smart-dash.insertGreaterThan', insertGt);
 }
 
 export function deactivate() { }
 
 function registerCommand(
-	context: ExtensionContext,
-	command: string,
-	callback: (editor: TextEditor, ...args: any[]) => any)
+    context: ExtensionContext,
+    command: string,
+    callback: (editor: TextEditor, ...args: any[]) => any)
 {
-	function editorCallback(...args: any[]) {
-		const editor = window.activeTextEditor;
-		if (!editor) {
-			return;
-		}
+    function editorCallback(...args: any[]) {
+        const editor = window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
 
-		const doc = editor.document;
-		const pos = editor.selection.start;
+        const doc = editor.document;
+        const pos = editor.selection.start;
 
-		return callback(editor, doc, pos, ...args);
-	}
-	context.subscriptions.push(
-			commands.registerCommand(command, editorCallback));
+        return callback(editor, doc, pos, ...args);
+    }
+    context.subscriptions.push(
+            commands.registerCommand(command, editorCallback));
 }
 
 async function insert(editor: TextEditor, doc: TextDocument, pos: Position) {
-	const identRegEx = /\w/;
+    const identRegEx = /\w/;
 
-	if (!smartDashIsAllowed(doc, pos)) {
-		type('-');
-		return;
-	}
+    if (!smartDashIsAllowed(doc, pos)) {
+        type('-');
+        return;
+    }
 
-	await fixupCLike(editor, doc, pos);
-	if (charsBehind(doc, pos, 1).match(identRegEx)) {
-		await type('_');
-	} else {
-		await type('-');
-	}
+    await fixupCLike(editor, doc, pos);
+    if (charsBehind(doc, pos, 1).match(identRegEx)) {
+        await type('_');
+    } else {
+        await type('-');
+    }
 }
 
 async function insertGt(editor: TextEditor, doc: TextDocument, pos: Position) {
-	if (smartDashIsAllowed(doc, pos)) {
-		await fixupCLike(editor, doc, pos);
-	}
-	await type('>');
+    if (smartDashIsAllowed(doc, pos)) {
+        await fixupCLike(editor, doc, pos);
+    }
+    await type('>');
 }
 
 async function fixupCLike(editor: TextEditor, doc: TextDocument, pos: Position)
 {
-	if (!languageIsCLike(doc))
-	{
-		return;
-	}
+    if (!languageIsCLike(doc))
+    {
+        return;
+    }
 
-	if (charsBehindEqual(doc, pos, '_')) {
-		await replaceLeft(editor, '-');
-	} else if (charsBehindEqual(doc, pos, '--')) {
-		await replaceLeft(editor, '_-');
-	}
+    if (charsBehindEqual(doc, pos, '_')) {
+        await replaceLeft(editor, '-');
+    } else if (charsBehindEqual(doc, pos, '--')) {
+        await replaceLeft(editor, '_-');
+    }
 }
 
 async function type(text: string) {
-	await commands.executeCommand('type', {text: text});
+    await commands.executeCommand('type', {text: text});
 }
 
 async function replaceLeft(editor: TextEditor, chars: string) {
-	const end = editor.selection.start;
-	const start = end.translate(undefined, -chars.length);
-	const range = new Range(start, end);
+    const end = editor.selection.start;
+    const start = end.translate(undefined, -chars.length);
+    const range = new Range(start, end);
 
-	await editor.edit(
-		editBuilder => editBuilder.replace(range, chars),
-		{undoStopBefore: false, undoStopAfter: false});
+    await editor.edit(
+        editBuilder => editBuilder.replace(range, chars),
+        {undoStopBefore: false, undoStopAfter: false});
 }
 
 function smartDashIsAllowed(doc: TextDocument, pos: Position): boolean {
-	return smartDashEnabled(doc) && !inVerbatimText(doc, pos);
+    return smartDashEnabled(doc) && !inVerbatimText(doc, pos);
 }
 
 function smartDashEnabled(doc: TextDocument) {
-	return languageIsInConfigParam(doc, "languages");
+    return languageIsInConfigParam(doc, "languages");
 }
 
 function languageIsCLike(doc: TextDocument) {
-	return languageIsInConfigParam(doc, "cLikeLanguages");
+    return languageIsInConfigParam(doc, "cLikeLanguages");
 }
 
 function languageIsInConfigParam(doc: TextDocument, param: string) {
-	let languages: Array<string> =
-		workspace.getConfiguration("smart-dash").get(param) || [];
-	return languages.includes(doc.languageId);
+    let languages: Array<string> =
+        workspace.getConfiguration("smart-dash").get(param) || [];
+    return languages.includes(doc.languageId);
 }
 
 function inVerbatimText(doc: TextDocument, pos: Position) {
-	const verbatimScopes = ['string', 'comment', 'numeric'];
+    const verbatimScopes = ['string', 'comment', 'numeric'];
 
-	let scopes = syntacticScopes(doc, pos);
-	let parts = scopes.map(scope => scope.split('.')).flat();
-	let verbatim = parts.some(part => verbatimScopes.includes(part));
+    let scopes = syntacticScopes(doc, pos);
+    let parts = scopes.map(scope => scope.split('.')).flat();
+    let verbatim = parts.some(part => verbatimScopes.includes(part));
 
-	return verbatim;
+    return verbatim;
 }
 
 function syntacticScopes(doc: TextDocument, pos: Position): string[]
 {
-	let hscopes = extensions.getExtension('draivin.hscopes')?.exports;
-	return hscopes?.getScopeAt(doc, pos)?.scopes || [];
+    let hscopes = extensions.getExtension('draivin.hscopes')?.exports;
+    return hscopes?.getScopeAt(doc, pos)?.scopes || [];
 }
 
 function charsBehindEqual(doc: TextDocument,
-	pos: Position,
-	chars: string): boolean
+    pos: Position,
+    chars: string): boolean
 {
-	return charsBehind(doc, pos, chars.length) === chars;
+    return charsBehind(doc, pos, chars.length) === chars;
 }
 
 function charsBehind(doc: TextDocument, pos: Position, chars: number): string
 {
-	let stringStart = doc.positionAt(doc.offsetAt(pos) - chars);
-	let range = new Range(stringStart, pos);
-	return doc.getText(range);
+    let stringStart = doc.positionAt(doc.offsetAt(pos) - chars);
+    let range = new Range(stringStart, pos);
+    return doc.getText(range);
 }
